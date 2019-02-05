e511d296c806c7367bde75ebe02f1a4bd5ef6a83
MarkTHayes <markh4387@gmail.com>
2019-02-05 21:57:43 +0000

    Key collectable

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+
+    public static LevelManager instance;
+
+    private GameObject player;
+    private GameObject door;
+    private GameObject key;
+    private GameObject doorSwitch;
+
+    public float speed;
+
+    public bool hasKey;
+    public bool hittingSwitch;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        door = GameObject.FindGameObjectWithTag("Door");
+        key = GameObject.FindGameObjectWithTag("Key");
+        doorSwitch = GameObject.FindGameObjectWithTag("Switch");
+
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        player.transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
+
+        InputManager();
+
+		
+	}
+
+
+    public void PlayerHitKey()
+    {
+
+        hasKey = true;
+        key.SetActive(false);
+
+    }
+
+    public void InputManager()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && hasKey && hittingSwitch)
+        {
+
+
+
+        }
+
+
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        
+        if (col.gameObject.tag == "Key")
+        {
+
+            LevelManager.instance.PlayerHitKey();
+
+        }
+
+        if (col.gameObject.tag == "Switch")
+        {
+
+            LevelManager.instance.hittingSwitch = true;
+
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Switch")
+        {
+
+            LevelManager.instance.hittingSwitch = false;
+
+        }
+
+    }
+
+
+}
